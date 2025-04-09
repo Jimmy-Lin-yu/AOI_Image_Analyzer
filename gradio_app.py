488d7867b84d4277f3749a79e9f3053e8c405a7a
Jimmy <gogokobe0922@gmail.com>
2025-04-09 06:04:41 +0000

    連接7860 port

--- gradio_app.py
+++ gradio_app.py
@@ -224,4 +224,4 @@ with gr.Blocks() as demo:
 
     backup_btn.click(backup_csv, outputs=backup_result)
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=7860)
